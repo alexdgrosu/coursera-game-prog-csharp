4f5778dc733e1d641a72ee9b4e57e7540b00f365
Alexandru D. Grosu <alexandru.d.grosu@gmail.com>
2018-01-26 19:07:05 +0200

    Complete Lab01.

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("My name is Frodo Baggins.");
+            System.Console.WriteLine("My nemesis is Sauron.");
+            System.Console.WriteLine("My favorite three games are: StarLancer, Freelancer and Squad.");
+        }
+    }
+}
